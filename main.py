8a1939bb0171b59b8acdd908085ddde8cc7d4ad4
raoroshanr <rao.roshan.r@gmail.com>
2026-07-08 11:43:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ except Exception:                                  # pragma: no cover
     FPDF = object
     _PDF_OK = False
 
-APP_VERSION = "deepseego-v48"
+APP_VERSION = "deepseego-v50"
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
